51a1d25ac02ead2fa496a5c6e171a8b947d830ad
trithecoder <triyelnewry@gmail.com>
2023-10-29 02:47:03 -0400

    Netflix-Project//First Release

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./netflix logo.png";
+import logo1 from "./Netflix img.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img className="logo" src={logo}></img>
+      <header className="App-header"></header>
+      <div>
+        <section className="sign-in-section">
+          <div className="form-container">
+            <form className="form" action="">
+              <h1 className="form-header">Sign in</h1>
+              <div className="input-box">
+                <input className="email-box" placeholder="Email or phone number" name="email-input-box" type="email" required></input>
+              </div>
+              <div className="input-box">
+                <input className="password-box" placeholder="Password" type="password" name="password-input-box" required></input>
+              </div>
+              <button className="form-button">Sign in</button>
+              <p>
+              <span className="first-time-using-netflix">First time using Netflix?</span> <a className="create-an-acc" href='https://help.netflix.com/en/node/112419'>Create an account</a>
+            </p>
+            </form>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
