3bacd6907973598a89b643815b64e7764b72b8b8
trithecoder <triyelnewry@gmail.com>
2023-10-29 12:12:09 -0400

    Updated Mobile and Tablet/Responsive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from "./netflix logo.png";
 import logo1 from "./Netflix img.jpg";
 import "./App.css";
+import "./AppMobile.css";
 
-<style src={'https://kit.fontawesome.com/81485fa12e.js'} defer></style>
 
 function App() {
   return (
@@ -15,19 +15,39 @@ function App() {
             <form className="form" action="">
               <h1 className="form-header">Sign in</h1>
               <div className="input-box">
-                <input className="email-box" placeholder="Email or phone number" name="email-input-box" type="email" required></input>
+                <input
+                  className="email-box"
+                  placeholder="Email or phone number"
+                  name="email-input-box"
+                  type="email"
+                  required
+                ></input>
               </div>
               <div className="input-box">
-                <input className="password-box" placeholder="Password" type="password" name="password-input-box" required></input>
+                <input
+                  className="password-box"
+                  placeholder="Password"
+                  type="password"
+                  name="password-input-box"
+                  required
+                ></input>
               </div>
               <button className="form-button">Sign in</button>
               <p>
-              <i class="fa-brands fa-github"></i>
-              <i class="fa-brands fa-google"></i>
+                <i class="fa-brands fa-github"></i>
+                <i class="fa-brands fa-google"></i>
               </p>
               <p>
-              <span className="first-time-using-netflix">First time using Netflix?</span> <a className="create-an-acc" href='https://help.netflix.com/en/node/112419'>Create an account</a>
-            </p>
+                <span className="first-time-using-netflix">
+                  First time using Netflix?
+                </span>{" "}
+                <a
+                  className="create-an-acc"
+                  href="https://help.netflix.com/en/node/112419"
+                >
+                  Create an account
+                </a>
+              </p>
             </form>
           </div>
         </section>
